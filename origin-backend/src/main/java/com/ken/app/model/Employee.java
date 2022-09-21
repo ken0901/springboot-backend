76a0573919a4e87ef1bd7dd419dfdd64a303d82b
ken0901 <ken5260715@gmail.com>
2022-09-20 23:14:30 -0700

    Add Employee model and repository

---
+++ origin-backend/src/main/java/com/ken/app/model/Employee.java
@@ -0,0 +1,27 @@
+package com.ken.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+}
