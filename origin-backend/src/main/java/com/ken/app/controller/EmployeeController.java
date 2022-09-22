3fba504386faf28c8f4915773ef197f074b91656
ken0901 <ken5260715@gmail.com>
2022-09-21 23:19:39 -0700

    Add cors(Cross-Origin Resource Sharing) and create employees

--- origin-backend/src/main/java/com/ken/app/controller/EmployeeController.java
+++ origin-backend/src/main/java/com/ken/app/controller/EmployeeController.java
@@ -3,12 +3,11 @@ package com.ken.app.controller;
 import com.ken.app.model.Employee;
 import com.ken.app.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -19,5 +18,11 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+
+    // Create employee rest api
+    @PostMapping("employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
     }
 }
