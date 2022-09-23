59821f1e340ecfc07c1a54c36ea50efbe4c2bea5
ken0901 <ken5260715@gmail.com>
2022-09-22 21:45:47 -0700

    Add UPDATE employee by id

--- origin-backend/src/main/java/com/ken/app/controller/EmployeeController.java
+++ origin-backend/src/main/java/com/ken/app/controller/EmployeeController.java
@@ -22,17 +22,31 @@ public class EmployeeController {
         return employeeRepository.findAll();
     }
 
-    // Create employee rest api
-    @PostMapping("employees")
+    // CREATE employee rest api
+    @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
     }
 
-    //GET employee by id rest api
+    // GET employee by id rest api
     @GetMapping("/employees/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
         Employee employee = employeeRepository.findById(id).
                 orElseThrow(()-> new ResourceNotFoundException("Employee not exist with id : "+ id));
         return ResponseEntity.ok(employee);
+    }
+
+    // UPDATE employee rest api
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetail){
+        Employee employee = employeeRepository.findById(id).
+                orElseThrow(()-> new ResourceNotFoundException("Employee not exist with id : "+ id));
+
+        employee.setFirstName(employeeDetail.getFirstName());
+        employee.setLastName(employeeDetail.getLastName());
+        employee.setEmailId(employeeDetail.getEmailId());
+
+        Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
     }
 }
